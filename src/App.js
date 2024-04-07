93922fda819851fea409a8d44d3608d06c58a14f
bobbyhadz <opmftw@gmail.com>
2024-04-07 11:43:17 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h2>Hello world</h2>
+    </div>
+  );
+};
+
+// 👇️ set display name
+App.displayName = 'MyApp';
+
+export default App;
